c3f19b4abcb922c8bb35178a93c05f041e98d6e0
Michael <28543330+Schello805@users.noreply.github.com>
2024-12-18 18:17:16 +0100

    Initial Commit

---
+++ Tribal/TribalApp.swift
@@ -0,0 +1,17 @@
+//
+//  TribalApp.swift
+//  Tribal
+//
+//  Created by Michael Schellenberger on 18.12.24.
+//
+
+import SwiftUI
+
+@main
+struct TribalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TribalTests/TribalTests.swift
@@ -0,0 +1,17 @@
+//
+//  TribalTests.swift
+//  TribalTests
+//
+//  Created by Michael Schellenberger on 18.12.24.
+//
+
+import Testing
+@testable import Tribal
+
+struct TribalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
